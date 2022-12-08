fb6cb3114dbcdb29df2ceb9df3a5855e8500ca54
Sujeong Cho <112532259+sujeongcho@users.noreply.github.com>
2022-12-08 17:06:05 +0900

    Create cal_test.js

---
+++ practice/count/cal_test.js
@@ -0,0 +1,22 @@
+var count = 1;
+var countEl = document.getElementById("count");
+var total_count = document.getElementById("total_count");
+var total_count_view = document.getElementById("total_count_view");
+
+function plus(){
+    count++;
+    countEl.value = count;
+    total_count_view.value = (total_count.value * countEl.value).toLocaleString();
+}
+function minus(){
+    if(count > 1){
+        count--;
+        countEl.value = count;
+        total_count_view.value = (total_count_view.value - total_count.value).toLocaleString();
+    }
+}
+
+
+console.log(total_count_view.toLocaleString());
+
+
